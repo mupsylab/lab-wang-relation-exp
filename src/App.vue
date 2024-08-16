405ee1f67212d5a7c8d68091006ce8f09771ef68
mupsy <amm@mupsy.net>
2024-08-16 05:01:55 -0400

    不需要排除微信浏览器

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { RouterView, useRouter, useRoute } from 'vue-router';
-
-const router = useRouter();
-const route = useRoute();
-onMounted(() => {
-  const ua = navigator.userAgent.toLowerCase();
-  if (ua.match(/micromessenger/i)) {
-    if(route.name != "not-found") {
-      router.push({
-        name: "not-found",
-        params: {
-          pathMatch: [400]
-        },
-        hash: route.hash
-      });
-    }
-  }
-});
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
