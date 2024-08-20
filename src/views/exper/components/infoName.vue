9efc61e6c62fb045b959e043d838934ed54579dd
mupsycho <amm@mupsy.net>
2024-08-20 19:17:32 +0800

    ready

--- src/views/exper/components/infoName.vue
+++ src/views/exper/components/infoName.vue
@@ -2,54 +2,22 @@
     <div class="info-name-box">
         <div class="title-box">
             <div class="title">{{ props.name }}</div>
-            <div class="content">{{ props.desc }}</div>
+            <div class="desc">{{ props.desc }}</div>
         </div>
         <form class="question-box" action="" @submit="formSubmit">
-            <div class="text-input">
-                <div class="text">您与此人属于什么关系？</div>
-                <input type="text" name="relation" placeholder="请输入" v-model="info.relation.value" required />
-            </div>
-            <div class="text-input">
-                <div class="text">上次面对面互动是什么时候？</div>
-                <input type="text" name="ia_face" placeholder="请输入" v-model="info.ia_face.value" required />
-            </div>
-            <div class="text-input">
-                <div class="text">上一次网上互动是什么时候？</div>
-                <input type="text" name="ia_online" placeholder="请输入" v-model="info.ia_online.value" required />
-            </div>
-            <div class="radio-input">
-                <div class="text">性别是？</div>
-                <div class="radio" style="--size: 2">
-                    <label for="gender_male"><input type="radio" id="gender_male" name="gender" value="male" @click="info.gender.value = 'male'" required />男</label>
-                    <label for="gender_female"><input type="radio" id="gender_female" name="gender" value="female" @click="info.gender.value = 'female'" />女</label>
+            <div v-for="item1, i1 in questionList" class="q-box">
+                <div class="text">
+                    <div class="title">{{ item1.title }}</div>
+                    <div class="desc">{{ item1.desc }}</div>
                 </div>
-            </div>
-            <div class="text-input">
-                <div class="text">年龄</div>
-                <input type="number" name="age" min="1" max="99" v-model="info.age.value" required />
-            </div>
-            <div class="text-input">
-                <div class="text">爱好</div>
-                <input type="text" name="hobby" placeholder="请输入" v-model="info.hobby.value" required />
-            </div>
-            <div class="radio-input">
-                <div class="text">好事找ta的可能性（1表示绝对不可能，5表示绝对会找）</div>
-                <div class="radio" style="--size: 5">
-                    <label for="good_1"><input type="radio" id="good_1" name="good" value="1" @click="info.good.value = 1" required />1</label>
-                    <label for="good_2"><input type="radio" id="good_2" name="good" value="2" @click="info.good.value = 2" />2</label>
-                    <label for="good_3"><input type="radio" id="good_3" name="good" value="3" @click="info.good.value = 3" />3</label>
-                    <label for="good_4"><input type="radio" id="good_4" name="good" value="4" @click="info.good.value = 4" />4</label>
-                    <label for="good_5"><input type="radio" id="good_5" name="good" value="5" @click="info.good.value = 5" />5</label>
+                <div class="radio" v-if="item1.type == 'radio'">
+                    <label :for="`${i1}_${i2}`" v-for="item2, i2 in item1.choices">
+                        <input type="radio" :name="`${i1}`" :id="`${i1}_${i2}`" :value="`${i2}`" required />
+                        {{ item2 }}
+                    </label>
                 </div>
-            </div>
-            <div class="radio-input">
-                <div class="text">坏事找ta的可能性（1表示绝对不可能，5表示绝对会找）</div>
-                <div class="radio" style="--size: 5">
-                    <label for="bad_1"><input type="radio" id="bad_1" name="bad" value="1" @click="info.bad.value = 1" required />1</label>
-                    <label for="bad_2"><input type="radio" id="bad_2" name="bad" value="2" @click="info.bad.value = 2" />2</label>
-                    <label for="bad_3"><input type="radio" id="bad_3" name="bad" value="3" @click="info.bad.value = 3" />3</label>
-                    <label for="bad_4"><input type="radio" id="bad_4" name="bad" value="4" @click="info.bad.value = 4" />4</label>
-                    <label for="bad_5"><input type="radio" id="bad_5" name="bad" value="5" @click="info.bad.value = 5" />5</label>
+                <div class="number" v-if="item1.type == 'number'">
+                    <input type="number" :name="`${i1}`" min="1" max="99" required />
                 </div>
             </div>
             <input type="submit" hidden>
@@ -62,8 +30,6 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from 'vue';
-
 const emits = defineEmits(["endTrial"]);
 const props = defineProps<{
     name: string,
@@ -71,23 +37,29 @@ const props = defineProps<{
     leave: number
 }>();
 
-const info: { [key: string]: Ref<string | number> } = {
-    relation: ref(""),
-    ia_face: ref(""),
-    ia_online: ref(""),
-    gender: ref(""),
-    age: ref(0),
-    hobby: ref(""),
-    good: ref(0),
-    bad: ref(0),
-};
+const questionList = [
+    { type: "radio", title: "你最近与这个人互动是什么时候？", choices: ["过去1周内", "1周前但在过去1个月内", "1个月前但在过去6个月内", "6个月前"], require: true },
+    { type: "radio", title: "你们的关系是什么？", choices: ["朋友", "同学", "家人", "其他"], require: true },
+    { type: "number", title: "这个人多大了？", desc: "请为每个人输入一个数字。如果您不确定，请输入您猜测的年龄。", require: true },
+    { type: "radio", title: "这个人的性别是什么？", choices: ["男", "女", "其他"], require: true },
+    { type: "radio", title: "这个人和你是同一种族，还是不同种族？", choices: ["相同种族", "不同种族", "不确定"], require: true },
+    { type: "radio", title: "你有多喜欢这个人？", choices: ["非常不喜欢", "有点不喜欢", "一般", "有点喜欢", "非常喜欢"], require: true },
+    { type: "radio", title: "当好事发生时，您去找这个人的可能性有多大？", choices: ["非常不可能", "不太可能", "中立", "可能", "非常可能"], require: true },
+    { type: "radio", title: "当一些糟糕的事情发生时，您去找这个人的可能性有多大？", choices: ["非常不可能", "不太可能", "中立", "可能", "非常可能"], require: true },
+];
+
 const submit = () => { (document.querySelector("input[type=submit]") as HTMLElement).click(); };
 const formSubmit = (e: Event) => {
     e.preventDefault();
 
-    const r: { [key: string]: string | number } = {};
-    Object.keys(info).forEach(k => {
-        r[k] = info[k].value;
+    const r: { [Key: string]: string } = {};
+    const doms = document.querySelectorAll("input");
+    doms.forEach((dom) => {
+        if (dom.type == "radio" && dom.checked) {
+            r[dom.name] = dom.value;
+        } else if (["number"].indexOf(dom.type) >= 0) {
+            r[dom.name] = dom.value;
+        }
     });
     emits("endTrial", r);
 
@@ -96,18 +68,6 @@ const formSubmit = (e: Event) => {
 </script>
 
 <style scoped>
-input[type=text], input[type=number] {
-    display: block;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
-    border: none;
-    font-size: 16px;
-    line-height: 36px;
-    color: var(--font-color);
-    background-color: var(--bg-color-input);
-}
-
 .question-box {
     display: block;
     width: 240px;
@@ -118,15 +78,38 @@ input[type=text], input[type=number] {
     overflow: hidden auto;
 }
 
-.question-box>div {
+.q-box {
     margin: 10px 0;
 }
 
-.question-box>div.radio-input .radio label {
-    display: inline-block;
-    width: calc(100% / var(--size));
-    text-align: center;
+.q-box .text .title {
+    font-size: 16px;
+}
+
+.q-box .text .desc {
+    font-size: 12px;
+    color: var(--font-desc);
+
+}
+
+.q-box .radio label {
+    display: block;
+    width: 100%;
+    text-align: left;
     cursor: pointer;
+}
+
+input[type=text],
+input[type=number] {
+    display: block;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+    border: none;
+    font-size: 16px;
+    line-height: 36px;
+    color: var(--font-color);
+    background-color: var(--bg-color-input);
 }
 
 .info-name-box {
